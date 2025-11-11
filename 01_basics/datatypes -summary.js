008b9766352e044063dca776bde4b8ba455cc43e
Pooja Mane <manepooja476@gmail.com>
2025-11-11 07:19:43 +0000

    datatypes-summary

--- 01_basics/datatypes -summary.js
+++ 01_basics/datatypes -summary.js
@@ -1,34 +1,47 @@
 // # primitive data types
 // string,number,boolean,null,undefined,symbol,BigInt
 
-const  score = 100
-const scoreValue = 100.3
+// const  score = 100
+// const scoreValue = 100.3
 
-const IsLoggedIn = false
-const outsideTemp = null
-let userEmail;
+// const IsLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
 //console.log(id === anotherId);
 
-const bigNumber = 234567899876543456n
-console.log(typeof bigNumber);
+// const bigNumber = 234567899876543456n
+// console.log(typeof bigNumber);
 
 
 //Reference(nonprimitive) data types
 //array,objects,functions
 
-const heros = ["shaktiman","doga","naagraj"]
+// const heros = ["shaktiman","doga","naagraj"]
 
-let myObje={
-    name:"poja"
-    age:21
+// let myObje={
+//     name:"poja"
+//     age :21
 
-}
+// }
 
-const myFunction = function() {
-    console.log("hello world");
+// const myFunction = function() {
+//     console.log("hello world");
     
     
-}
+// }
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Stack(primitive) , Heap(nonprimitive) 
+
+let myYoutubename = "poojasgoaloflife"
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+
