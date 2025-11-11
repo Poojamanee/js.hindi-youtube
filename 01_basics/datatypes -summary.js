22bb48c6ab2212a0396a6eba37e64fbd64afd0a4
Pooja Mane <manepooja476@gmail.com>
2025-11-11 07:03:40 +0000

    datatypes -summary

---
+++ 01_basics/datatypes -summary.js
@@ -0,0 +1,34 @@
+// # primitive data types
+// string,number,boolean,null,undefined,symbol,BigInt
+
+const  score = 100
+const scoreValue = 100.3
+
+const IsLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+//console.log(id === anotherId);
+
+const bigNumber = 234567899876543456n
+console.log(typeof bigNumber);
+
+
+//Reference(nonprimitive) data types
+//array,objects,functions
+
+const heros = ["shaktiman","doga","naagraj"]
+
+let myObje={
+    name:"poja"
+    age:21
+
+}
+
+const myFunction = function() {
+    console.log("hello world");
+    
+    
+}
